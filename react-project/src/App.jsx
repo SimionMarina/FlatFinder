124697e03fffd45871edda962c5a8ca5209160e7
SimionMarina <simion.marina99@gmail.com>
2024-08-07 19:33:57 +0300

    getting there

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,18 +1,5 @@
-import Home from "./COMPONENTS/HOME/Home"
-
-
 function App() {
-  
-
-  return (
-    <>
-<<<<<<< HEAD
-      <Home></Home>
-=======
-
->>>>>>> 510eb1e2bf941bc624ebafccb4f6c506865c8bae
-    </>
-  )
+  return <></>;
 }
 
-export default App
+export default App;
--- react-project/src/COMPONENTS/AUTH/Login.jsx
+++ react-project/src/COMPONENTS/AUTH/Login.jsx
@@ -1,151 +1,126 @@
-import React from 'react'
-import 'boxicons/css/boxicons.min.css';
-import Box from '@mui/material/Box';
-import EmailIcon from '@mui/icons-material/Email';
-import LockIcon from '@mui/icons-material/Lock';
-import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
-import InputAdornment from '@mui/material/InputAdornment';
-import Input from '@mui/material/Input';
-import Checkbox from '@mui/material/Checkbox';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
-import GoogleIcon from '../../assets/GOOGLE-ICON.svg';
+import React from "react";
+import "boxicons/css/boxicons.min.css";
+import Box from "@mui/material/Box";
+import EmailIcon from "@mui/icons-material/Email";
+import LockIcon from "@mui/icons-material/Lock";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import Input from "@mui/material/Input";
+import Checkbox from "@mui/material/Checkbox";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import GoogleIcon from "../../assets/GOOGLE-ICON.svg";
 
-
-
-
-
-const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
-
-
-
+const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 function Login() {
   return (
     <div>
-        <div className="background__division">
-
-            <div className='image__section'>
-                <h2 className='hero__title'>Turn Your Ideas into Reality</h2>
-                <p className='hero__paragraph'>Start for free and get attractive offers from the community</p>
-            </div>
-
-            <div className="form__section">
-
-               <div className='form__details'>
-                    <div className='logo__section'>
-                        <i className='bx bxs-home-heart'></i>
-                        <h4 className='company__title'>FlatFinder</h4>
-                    </div>
-    
-                    <div className="form__header">
-                        <h1 className='form__title'>Login</h1>
-                        <p className='form__info'>Welcome back! Please enter your details.</p>
-                    </div>
-    
-    
-                    <div >
-                        <Box sx={{ '& > :not(style)': { m: 0 } }} className='inputs__side'>
-                                   
-                                    <FormControl variant="standard" sx={{ width: '100%'}} >
-                                <InputLabel htmlFor="input-with-icon-adornment"  sx={{color:"black"}}>
-                                Email
-                                </InputLabel>
-                                <Input
-                                    id="input-with-icon-adornment"
-                                    startAdornment={
-                                    <InputAdornment position="start">
-                                    <EmailIcon sx={{color:"black"}}/>
-                                    </InputAdornment>
-                                    }
-                                />
-                            </FormControl>
-        
-                            <FormControl variant="standard" sx={{ width: '100%'}}>
-                                <InputLabel htmlFor="input-with-icon-adornment" sx={{color:"black"}}>
-                                Password
-                                </InputLabel>
-                                <Input  
-                                    type='password'
-                                    id="input-with-icon-adornment"
-                                    startAdornment={
-                                    <InputAdornment position="start">
-                                    <LockIcon sx={{color:"black"}}/>
-                                    </InputAdornment>
-                                    }
-                                />
-                            </FormControl>
-                            
-                        </Box>
-                        <div className='remember__forgot'>
-                            <p className='checkbox__text'>      
-                                <Checkbox {...label} className='checkbox' />
-                                Remember me
-                            </p>
-                            <p className='forgot__password'>Forgot Password?</p>
-                        </div>
-
-                        <Stack direction="row" spacing={2}>
-                            <Button className='login__button' variant="contained" sx={{backgroundColor:"black", width: "100%", height:"50px"}} >LogIn</Button>
-                        </Stack>
-
-                        <div class="separator">
-                            <div class="line"></div>
-                            <p class="text">or</p>
-                        </div>
-
-                        <div className='icon-container'>
-                            <img src={GoogleIcon} className='icon' alt="Google Icon" />
-                            Sign In With Google
-                        </div>
-
-                        <div className='navigate'>
-                           <p className='no__account'> Don't have an account?</p> 
-                           <a href="#">Sign Up here</a>
-                           
-                        </div>
-
-
-                    </div>
-               </div>
-
-            </div>
+      <div className="background__division">
+        <div className="image__section">
+          <h2 className="hero__title">Turn Your Ideas into Reality</h2>
+          <p className="hero__paragraph">
+            Start for free and get attractive offers from the community
+          </p>
         </div>
-        
-    </div>
 
-  )
+        <div className="form__section">
+          <div className="form__details">
+            <div className="logo__section">
+              <i className="bx bxs-home-heart"></i>
+              <h4 className="company__title">FlatFinder</h4>
+            </div>
+
+            <div className="form__header">
+              <h1 className="form__title">Login</h1>
+              <p className="form__info">
+                Welcome back! Please enter your details.
+              </p>
+            </div>
+
+            <div>
+              <Box
+                sx={{ "& > :not(style)": { m: 0 } }}
+                className="inputs__side"
+              >
+                <FormControl variant="standard" sx={{ width: "100%" }}>
+                  <InputLabel
+                    htmlFor="input-with-icon-adornment"
+                    sx={{ color: "black" }}
+                  >
+                    Email
+                  </InputLabel>
+                  <Input
+                    id="input-with-icon-adornment"
+                    startAdornment={
+                      <InputAdornment position="start">
+                        <EmailIcon sx={{ color: "black" }} />
+                      </InputAdornment>
+                    }
+                  />
+                </FormControl>
+
+                <FormControl variant="standard" sx={{ width: "100%" }}>
+                  <InputLabel
+                    htmlFor="input-with-icon-adornment"
+                    sx={{ color: "black" }}
+                  >
+                    Password
+                  </InputLabel>
+                  <Input
+                    type="password"
+                    id="input-with-icon-adornment"
+                    startAdornment={
+                      <InputAdornment position="start">
+                        <LockIcon sx={{ color: "black" }} />
+                      </InputAdornment>
+                    }
+                  />
+                </FormControl>
+              </Box>
+              <div className="remember__forgot">
+                <p className="checkbox__text">
+                  <Checkbox {...label} className="checkbox" />
+                  Remember me
+                </p>
+                <p className="forgot__password">Forgot Password?</p>
+              </div>
+
+              <Stack direction="row" spacing={2}>
+                <Button
+                  className="login__button"
+                  variant="contained"
+                  sx={{
+                    backgroundColor: "black",
+                    width: "100%",
+                    height: "50px",
+                  }}
+                >
+                  LogIn
+                </Button>
+              </Stack>
+
+              <div className="separator">
+                <div className="line"></div>
+                <p className="text">or</p>
+              </div>
+
+              <div className="icon-container">
+                <img src={GoogleIcon} className="icon" alt="Google Icon" />
+                Sign In With Google
+              </div>
+
+              <div className="navigate">
+                <p className="no__account"> Don't have an account?</p>
+                <a href="#">Sign Up here</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Login
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default Login;
