cb41af37e71aa16da894b3a0e6959fea1132c5db
SimionMarina <simion.marina99@gmail.com>
2024-08-10 12:17:48 +0300

    dwadwaawa

--- react-project/src/CONTEXT/authContext.jsx
+++ react-project/src/CONTEXT/authContext.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
 import { useContext, useEffect, useState, createContext } from "react";
 import { auth } from "../firebase";
 import { onAuthStateChanged } from "firebase/auth";
