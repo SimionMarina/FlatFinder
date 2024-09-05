4ec8e49e398712474019635ec39b28f52a17d7a2
SimionMarina <simion.marina99@gmail.com>
2024-09-05 19:37:39 +0300

    inbox change

--- react-project/src/COMPONENTS/INBOX/Inbox.jsx
+++ react-project/src/COMPONENTS/INBOX/Inbox.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useAuth } from "../../CONTEXT/authContext";
-import { db } from "../../firebase"; 
+import { db } from "../../firebase";
 import { doc, getDoc, updateDoc, arrayUnion } from "firebase/firestore";
 import {
   Typography,
@@ -16,15 +16,16 @@ import Header from "../HEADER/Header";
 import "./Inbox.css";
 import showToastr from "../../SERVICES/toaster-service";
 import { ToastContainer } from "react-toastify";
-import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
+import KeyboardReturnIcon from "@mui/icons-material/KeyboardReturn";
 import { useNavigate } from "react-router-dom";
 
 const Inbox = () => {
   const [groupedMessages, setGroupedMessages] = useState({});
   const [flatDetails, setFlatDetails] = useState({});
   const [reply, setReply] = useState({});
+  const [showInput, setShowInput] = useState({});
   const { currentUser } = useAuth();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchMessages = async () => {
@@ -91,88 +92,119 @@ const Inbox = () => {
         messages: arrayUnion(replyMessage),
       });
       setReply({ ...reply, [senderUid]: "" }); // Clear the reply input
+      setShowInput({ ...showInput, [senderUid]: false }); // Hide input after sending
       showToastr("success", "Reply sent successfully!");
     } catch (error) {
       console.error("Error sending reply:", error);
     }
   };
 
+  const toggleInputVisibility = (senderUid) => {
+    setShowInput({ ...showInput, [senderUid]: !showInput[senderUid] });
+  };
+
   return (
     <>
-          <ToastContainer></ToastContainer>
+      <ToastContainer></ToastContainer>
       <div className="background__container__home">
-      <Header />
-      <KeyboardReturnIcon
-                onClick={() => navigate("/")}
-                sx={{
-                 color:"gray",
-                 margin:"10px 20px",
-                 cursor:"pointer"
-                }}></KeyboardReturnIcon>
+        <Header />
+        <KeyboardReturnIcon
+          onClick={() => navigate("/")}
+          sx={{
+            color: "gray",
+            margin: "10px 20px",
+            cursor: "pointer",
+          }}
+        ></KeyboardReturnIcon>
 
-      <Container>
-        <Typography sx={{display: "flex", justifyContent: "center"}} gutterBottom >
-          <h2 className="inbox__title">MESSAGES</h2>
-        </Typography>
-        <Stack className="stack__container" spacing={2}>
-          {Object.keys(groupedMessages).length === 0 ? (
-            <Typography sx={{color:"red", backgroundColor:"rgba(0,0,0,0.7)", padding:"10px 20px"}}>No messages found.</Typography>
-          ) : (
-            Object.keys(groupedMessages).map((senderUid) => (
-              <Card className="card__container"  key={senderUid}>
-                <CardContent className="card__content">
-                  <Typography variant="h6">
-                    From: {groupedMessages[senderUid][0].senderName}
-                  </Typography>
-                  {groupedMessages[senderUid].map((message, index) => {
-                    const flat = flatDetails[message.flatId];
-                    return (
-                      <div key={index} style={{ marginBottom: "10px" }}>
-                        <Typography variant="body1">{message.content}</Typography>
-                        {flat && (
-                          <Typography variant="caption" color="textSecondary">
-                            Location: {flat.city}, {flat.streetName} {flat.streetNumber}
+        <Container>
+          <Typography
+            sx={{ display: "flex", justifyContent: "center" }}
+            gutterBottom
+          >
+            <h2 className="inbox__title">MESSAGES</h2>
+          </Typography>
+          <Stack className="stack__container" spacing={2}>
+            {Object.keys(groupedMessages).length === 0 ? (
+              <Typography
+                sx={{
+                  color: "red",
+                  backgroundColor: "rgba(0,0,0,0.7)",
+                  padding: "10px 20px",
+                }}
+              >
+                No messages found.
+              </Typography>
+            ) : (
+              Object.keys(groupedMessages).map((senderUid) => (
+                <Card
+                  sx={{ background: "rgba(0,0,0,0.4)" }}
+                  className="card__container"
+                  key={senderUid}
+                >
+                  <CardContent className="card__content">
+                    <Typography variant="h6">
+                      From: {groupedMessages[senderUid][0].senderName}
+                    </Typography>
+                    {groupedMessages[senderUid].map((message, index) => {
+                      const flat = flatDetails[message.flatId];
+                      return (
+                        <div key={index} style={{ marginBottom: "10px" }}>
+                          <Typography variant="body1">
+                            {message.content}
                           </Typography>
-                        )}
-                        <br />
-                        <Typography variant="caption" color="textSecondary">
-                          Sent on:{" "}
-                          {new Date(
-                            message.createdAt.seconds * 1000
-                          ).toLocaleString()}
-                        </Typography>
-                      </div>
-                    );
-                  })}
-                </CardContent>
-                <CardActions sx={{
-                  display: "flex",
-                  justifyContent:"center",
-                  alignItems:"center",
-                }}>
-                  <TextField
-                    heig
-                    className="message__input"
-                    label="Reply"
-                    variant="outlined"
-                    value={reply[senderUid] || ""}
-                    onChange={(e) => handleReplyChange(senderUid, e.target.value)}
-                  />
-                  <Button
-                  sx={{
-                    background:"blueviolet"
-                  }}
-                    variant="contained"
-                    onClick={() => handleSendReply(senderUid)}
+                          {flat && (
+                            <Typography variant="caption" color="textSecondary">
+                              Flat Location: {flat.city}, {flat.streetName}{" "}
+                              {flat.streetNumber}
+                            </Typography>
+                          )}
+                          <br />
+                          <Typography variant="caption" color="textSecondary">
+                            Sent on:{" "}
+                            {new Date(
+                              message.createdAt.seconds * 1000
+                            ).toLocaleString()}
+                          </Typography>
+                        </div>
+                      );
+                    })}
+                  </CardContent>
+                  <CardActions
+                    className="card__actions"
+                    sx={{ display: "flex", justifyContent: "flex-end" }}
                   >
-                    Send Reply
-                  </Button>
-                </CardActions>
-              </Card>
-            ))
-          )}
-        </Stack>
-      </Container>
+                    {showInput[senderUid] && (
+                      <TextField
+                        className="message__input"
+                        label="Reply"
+                        variant="outlined"
+                        value={reply[senderUid] || ""}
+                        onChange={(e) =>
+                          handleReplyChange(senderUid, e.target.value)
+                        }
+                      />
+                    )}
+                    <Button
+                      sx={{
+                        width: "200px",
+                        background: "blueviolet",
+                      }}
+                      variant="contained"
+                      onClick={() =>
+                        showInput[senderUid]
+                          ? handleSendReply(senderUid)
+                          : toggleInputVisibility(senderUid)
+                      }
+                    >
+                      {showInput[senderUid] ? "Send Reply" : "Reply"}
+                    </Button>
+                  </CardActions>
+                </Card>
+              ))
+            )}
+          </Stack>
+        </Container>
       </div>
     </>
   );
